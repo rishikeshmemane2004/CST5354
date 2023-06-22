507575ff6bc79d0be729b599def1b9800486b2f4
rishimemane <rishimemane@gmail.com>
2023-06-22 13:31:44 +0530

    second commit

--- src/test/java/day22jun23/Test1.java
+++ src/test/java/day22jun23/Test1.java
@@ -4,5 +4,8 @@ public class Test1 {
 public static void main(String[] args) {
 	System.out.println("Hi first class first line");
 	int i = 10;
+	float a = 12.23f;
+	for(a=0.1f;a<=0.9;a+=0.1f)
+		System.out.println(a);
 }
 }
